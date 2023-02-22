641848aa9c234a6cbdc1d07388b42598db3025ea
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-02-22 12:04:39 +0300

    add exception package

---
+++ src/main/java/com/dxvalley/epassbook/exceptions/ApiRequestException.java
@@ -0,0 +1,8 @@
+package com.dxvalley.epassbook.exceptions;
+
+public class ApiRequestException extends RuntimeException {
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+}
