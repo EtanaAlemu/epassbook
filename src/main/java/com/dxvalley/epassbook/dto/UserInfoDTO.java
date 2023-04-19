a4a4032e1e57e46d0ff4b45673018bbf7482b237
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-04-19 15:42:01 +0300

    Add DTO for mapping incoming responses and requests.

---
+++ src/main/java/com/dxvalley/epassbook/dto/UserInfoDTO.java
@@ -0,0 +1,15 @@
+package com.dxvalley.epassbook.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfoDTO {
+    private UserDTO userInfo;
+}
+
+
+
+
