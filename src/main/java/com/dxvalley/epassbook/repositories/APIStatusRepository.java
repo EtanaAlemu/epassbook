e0c9f6f7f1f35fa102b59797040f928f993255a7
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-04-19 15:46:29 +0300

    Add APIStatus for disabling or enabling endpoints.

---
+++ src/main/java/com/dxvalley/epassbook/repositories/APIStatusRepository.java
@@ -0,0 +1,7 @@
+package com.dxvalley.epassbook.repositories;
+
+import com.dxvalley.epassbook.models.APIStatus;
+import org.springframework.data.repository.CrudRepository;
+
+public interface APIStatusRepository extends CrudRepository<APIStatus, Short> {
+}
---
+++ src/main/java/com/dxvalley/epassbook/services/impl/APIStatusServiceImpl.java
@@ -0,0 +1,33 @@
+package com.dxvalley.epassbook.services.impl;
+
+import com.dxvalley.epassbook.models.APIStatus;
+import com.dxvalley.epassbook.repositories.APIStatusRepository;
+import com.dxvalley.epassbook.services.APIStatusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class APIStatusServiceImpl implements APIStatusService {
+    @Autowired
+    APIStatusRepository apiStatusRepository;
+    @Override
+    public APIStatus getApiStatus() {
+        return apiStatusRepository.findById((short) 1)
+                .orElseThrow(() ->new RuntimeException("Internal server error while fetching API Status"));
+    }
+
+    @Override
+    public APIStatus updateApiStatus(APIStatus apiStatus) {
+        APIStatus result = getApiStatus();
+        result.setAtmCardEnabled(Optional.ofNullable(apiStatus.getAtmCardEnabled()).orElse(result.getAtmCardEnabled()));
+        result.setEarlyPayDayEnabled(Optional.ofNullable(apiStatus.getEarlyPayDayEnabled()).orElse(result.getEarlyPayDayEnabled()));
+        result.setAtmPinChangeEnabled(Optional.ofNullable(apiStatus.getAtmPinChangeEnabled()).orElse(result.getAtmPinChangeEnabled()));
+        result.setAccountStatementEnabled(Optional.ofNullable(apiStatus.getAccountStatementEnabled()).orElse(result.getAccountStatementEnabled()));
+        result.setCardLessCashEnabled(Optional.ofNullable(apiStatus.getCardLessCashEnabled()).orElse(result.getCardLessCashEnabled()));
+        result.setNewProductEnabled(Optional.ofNullable(apiStatus.getNewProductEnabled()).orElse(result.getNewProductEnabled()));
+
+        return apiStatusRepository.save(result);
+    }
+}
